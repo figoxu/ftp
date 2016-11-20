692b2d69472607621f86a247141d1cb088d678ea
jianhui xu <jianhui.xu@tendcloud.com>
2016-11-20 19:43:44 +0800

    Add MySql Info

--- start.sh
+++ start.sh
@@ -1,6 +1,6 @@
 export MYSQL_USER=root
-export MYSQL_PWD=4rfv%TGB
-export MYSQL_HOST=115.159.104.88
+export MYSQL_PWD=123
+export MYSQL_HOST=127.0.0.1
 export MYSQL_PORT=3306
 export MYSQL_NAME=figo_research
 export MYSQL_IDLE=1
