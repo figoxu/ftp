fa62d209ac16fe3cf9281f536ce274c23d1def84
figoxu <xujh945@gmail.com>
2017-01-01 07:03:59 +0800

    Refactor : Move User Dao Code Location

---
+++ ds-user.go
@@ -0,0 +1,57 @@
+package main
+
+import (
+	"github.com/astaxie/beego/orm"
+	"github.com/figoxu/Figo"
+	"github.com/quexer/utee"
+)
+
+func (p *DataSource) searchByAccount(Account string) *User {
+	defer Figo.Catch()
+	user := &User{}
+	err := p.MYSQL.Raw("SELECT id,account,password,basepath from user where Account=?", Account).QueryRow(user)
+	utee.Chk(err)
+	return user
+}
+
+func (p *DataSource) save(user *User) *User {
+	defer Figo.Catch()
+	_, err := p.MYSQL.Insert(user)
+	utee.Chk(err)
+	return user
+}
+
+func (p *DataSource) update(user *User) *User {
+	defer Figo.Catch()
+	_, err := p.MYSQL.Update(user)
+	utee.Chk(err)
+	return user
+}
+
+func (p *DataSource) delete(user *User) {
+	defer Figo.Catch()
+	_, err := p.MYSQL.Delete(user)
+	utee.Chk(err)
+}
+
+func (p *DataSource) search() []User {
+	defer Figo.Catch()
+	var users []User
+	_, err := p.MYSQL.Raw("SELECT id,account,password,basepath from user").QueryRows(&users)
+	utee.Chk(err)
+	return users
+}
+
+type UserDAO struct {
+	orm orm.Ormer
+}
+
+func (p *UserDAO) GetORM() orm.Ormer {
+	return p.orm
+}
+func (p *UserDAO) GetItemContainer() interface{} {
+	return &User{}
+}
+func (p *UserDAO) GetItemsContainer() interface{} {
+	return &[]User{}
+}
