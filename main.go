c52c88ae8185958f483b4eee63548b27cf3005dd
figoxu <xujh945@qq.com>
2017-07-13 09:28:24 +0800

    init v2

--- main.go
+++ main.go
@@ -9,7 +9,10 @@ import (
 	"io/ioutil"
 	"log"
 	"fmt"
+	"github.com/figoxu/utee"
 )
+
+var G_CFG config.Configer
 
 type DiskDriver struct {
 	cfg  config.Configer
@@ -18,31 +21,31 @@ type DiskDriver struct {
 }
 
 func (p *DiskDriver) Authenticate(user string, pass string) bool {
-	password := p.cfg.String(fmt.Sprint(user,"::pass"))
-	if pass!=password {
+	password := p.cfg.String(fmt.Sprint(user, "::pass"))
+	if pass != password {
 		return false;
 	}
 	p.user = user
-	p.path = p.cfg.String(fmt.Sprint(user,"::path"))
+	p.path = p.cfg.String(fmt.Sprint(user, "::path"))
 	return true
 }
 
-func (p *DiskDriver) RealPath(releativePath string)string {
-	return fmt.Sprint(p.path,releativePath)
+func (p *DiskDriver) RealPath(releativePath string) string {
+	return fmt.Sprint(p.path, releativePath)
 }
 
 func (p *DiskDriver) Bytes(path string) (bytes int) {
-	path=p.RealPath(path)
-	fileInfo,err:= os.Stat(path)
-	if err!=nil {
+	path = p.RealPath(path)
+	fileInfo, err := os.Stat(path)
+	if err != nil {
 		return 0
 	}
 	return int(fileInfo.Size())
 }
 
 func (p *DiskDriver) ModifiedTime(path string) (time.Time, error) {
-	path=p.RealPath(path)
-	fileInfo,err:= os.Stat(path)
+	path = p.RealPath(path)
+	fileInfo, err := os.Stat(path)
 	if err != nil {
 		return time.Now(), err
 	}
@@ -53,14 +56,17 @@ func (p *DiskDriver) ChangeDir(path string) bool {
 	return true
 }
 
-func (p *DiskDriver) DirContents(path string) ( []os.FileInfo) {
-	files := make([]os.FileInfo,0)
-	path = p.RealPath(path)
-	filepath.Walk(path, func(f string, info os.FileInfo, err error) error {
-		if info.IsDir() {
+func (p *DiskDriver) DirContents(path string) ([]os.FileInfo) {
+	files := make([]os.FileInfo, 0)
+	files = append(files, NewDirItem("."))
+	files = append(files, NewDirItem(".."))
+	filepath.Walk(p.RealPath(path), func(f string, info os.FileInfo, err error) error {
+		if info == nil ||  path== fmt.Sprint("/",info.Name()){
+			return nil
+		}else if info.IsDir() {
 			files = append(files, NewDirItem(info.Name()))
-		}else{
-			files = append(files, NewFileItem(info.Name(),int(info.Size())))
+		} else {
+			files = append(files, NewFileItem(info.Name(), int(info.Size())))
 		}
 		return nil
 	})
@@ -70,8 +76,8 @@ func (p *DiskDriver) DirContents(path string) ( []os.FileInfo) {
 func (p *DiskDriver) DeleteDir(path string) bool {
 	path = p.RealPath(path)
 	log.Printf("DeleteDir: %s", path)
-	f,err:=os.Lstat(path)
-	if err!=nil || !f.IsDir() {
+	f, err := os.Lstat(path)
+	if err != nil || !f.IsDir() {
 		return false
 	}
 	os.Remove(path)
@@ -81,8 +87,8 @@ func (p *DiskDriver) DeleteDir(path string) bool {
 func (p *DiskDriver) DeleteFile(path string) bool {
 	path = p.RealPath(path)
 	log.Printf("DeleteFile: %s", path)
-	f,err:=os.Lstat(path)
-	if err!=nil || f.IsDir() {
+	f, err := os.Lstat(path)
+	if err != nil || f.IsDir() {
 		return false
 	}
 	os.Remove(path)
@@ -92,38 +98,38 @@ func (p *DiskDriver) DeleteFile(path string) bool {
 func (p *DiskDriver) Rename(fromPath string, toPath string) bool {
 	oldPath := p.RealPath(fromPath)
 	newPath := p.RealPath(toPath)
-	return os.Rename(oldPath, newPath)!=nil
+	return os.Rename(oldPath, newPath) != nil
 }
 
 func (p *DiskDriver) MakeDir(path string) bool {
-	return os.Mkdir(p.RealPath(path), os.ModePerm)!=nil
+	return os.Mkdir(p.RealPath(path), os.ModePerm) != nil
 }
 
-func (p *DiskDriver) GetFile(path string) ( string,  error) {
-	path = p.RealPath( path)
-	b,err:=ioutil.ReadFile(path)
-	return string(b),err
+func (p *DiskDriver) GetFile(path string) (string, error) {
+	path = p.RealPath(path)
+	b, err := ioutil.ReadFile(path)
+	return string(b), err
 }
 func (p *DiskDriver) PutFile(destPath string, data io.Reader) bool {
-	destPath = p.RealPath( destPath)
+	destPath = p.RealPath(destPath)
 	log.Printf("PutFile: %s", destPath)
 	contents, err := ioutil.ReadAll(data)
 	if err != nil {
 		return false
 	}
 	var existFlag bool
-	if fileInfo,err:=os.Stat(destPath);err==nil {
+	if fileInfo, err := os.Stat(destPath); err == nil {
 		existFlag = true
 		if fileInfo.IsDir() {
 			return false
 		}
-	}else if os.IsNotExist(err) {
+	} else if os.IsNotExist(err) {
 		existFlag = false
 	}
 	if existFlag {
 		os.Remove(destPath)
 	}
-	ioutil.WriteFile(destPath,contents,os.ModePerm)
+	ioutil.WriteFile(destPath, contents, os.ModePerm)
 
 	return true
 }
@@ -134,17 +140,16 @@ func (p *DiskDriver) PutFile(destPath string, data io.Reader) bool {
 type DiskDriverFactory struct{}
 
 func (factory *DiskDriverFactory) NewDriver() (FTPDriver, error) {
-	driver := &DiskDriver{}
+	driver := &DiskDriver{
+		cfg:G_CFG,
+	}
 	return driver, nil
 }
 
 // it's alive!
 func main() {
-	factory := &DiskDriverFactory{}
-	ftpServer := NewFTPServer(&FTPServerOpts{Factory: factory })
-	err := ftpServer.ListenAndServe()
-	if err != nil {
-		log.Fatal("Error starting server!")
-		log.Fatal(err)
-	}
+	cfg, err := config.NewConfig("ini", "conf.ini")
+	utee.Chk(err)
+	G_CFG = cfg
+	utee.Chk(NewFTPServer(&FTPServerOpts{Factory: &DiskDriverFactory{} }).ListenAndServe())
 }
