eabfe83fb472201ff1d856050356a518a3b49ea3
figoxu <xujh945@qq.com>
2017-07-13 15:46:12 +0800

    clean last version

--- start.sh
+++
@@ -1,8 +0,0 @@
-export MYSQL_USER=root
-export MYSQL_PWD=4rfv%TGB
-export MYSQL_HOST=115.159.104.88
-export MYSQL_PORT=3306
-export MYSQL_NAME=figo_research
-export MYSQL_IDLE=1
-export MYSQL_ACTIVE=1
-./ftp
