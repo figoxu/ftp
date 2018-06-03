b74b7e01c6d74aebf15f16b8bbc6b9e2db62405d
figoxu <xujh945@qq.com>
2018-06-03 11:03:38 +0800

    增加默认跳转登录页的引导

--- admin/config/routes.rb
+++ admin/config/routes.rb
@@ -1,4 +1,5 @@
 Rails.application.routes.draw do
+  root :to => "login#index"
   get 'login/index'
 
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
