acb2ba26eafb209975a240623c4174a8cb32e72f
figoxu <xujh945@qq.com>
2018-06-03 16:39:34 +0800

    增加登陆接口

--- admin/app/controllers/login_controller.rb
+++ admin/app/controllers/login_controller.rb
@@ -1,4 +1,22 @@
 class LoginController < ApplicationController
   def index
   end
+
+  def login
+    puts params
+    @user = User.find_by(:name => params[:name])
+    if @user == nil
+      puts "hello"
+      flash.now[:error] = "用户不存在"
+      render 'index'
+    elsif @user.password != params[:password]
+      puts "world"
+      flash.now[:error] = "密码错误"
+      render 'index'
+    else
+      session[:user] = @user
+      # return "index"
+    end
+  end
+
 end
--- admin/config/routes.rb
+++ admin/config/routes.rb
@@ -1,6 +1,7 @@
 Rails.application.routes.draw do
   root :to => "login#index"
   get 'login/index'
+  post 'login/login'
 
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
